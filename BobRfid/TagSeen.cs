6480c7c5d71c2ac770a1d1fe93a162fb42044e51
Matt Michielsen <matt@michielsen.us>
2021-03-13 15:16:22 -0500

    Forgot TagSeen class

---
+++ BobRfid/TagSeen.cs
@@ -0,0 +1,12 @@
+﻿using Impinj.OctaneSdk;
+using System;
+
+namespace BobRfid
+{
+    public class TagSeen
+    {
+        public string Epc { get; set; }
+        public Tag Tag { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
